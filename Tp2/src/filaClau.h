24a458811e7c45a7b711f87c64d9bd49ef4a4d6a
Vinícius <omendes.vinicius@gmail.com>
2021-04-11 22:45:03 -0300

    commit on main

--- Tp2/src/filaClau.h
+++ Tp2/src/filaClau.h
@@ -5,21 +5,30 @@
 #ifndef FILACLAU_H
 #define FILACLAU_H
 
-typedef int TChave;
+//typedef int TChave;
 typedef struct TItem{
-    TChave chave;
-    int clausula;
-}TItem;
+    //TChave chave;
+    int linha;
+    int coluna;
+    int valor
+}TItem; 
+// é o item da lista, por exemplo: o item m[i][j] da matriz. Ou seja, deve conter o conteúdo.
+// No exemplo acima (mat[i][j]) o conteúdo seria : linha, coluna e o valor da posição.
 
 typedef struct Celula* Apontador;
 typedef struct Celula{
     TItem item;
     struct Celula* pProx;
 }TCelula;
+// tem como principal função "encapsular" o item, e gerar o apontador pro proximo item.
 
 typedef struct TFila{
     Apontador pFrente;
     Apontador pTras;
 }TFila; 
+// gera os identificadores da lista.
+
+// entretando, se apenas utilizarmos essa lista não poderemos obeter a matriz completa
+// precisaremos de uma lista de lista pra realizar esse processo
 
 #endif
