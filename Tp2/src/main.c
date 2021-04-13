1c2a9df259a10a73e10d4d4a6c2d1ff226541344
Artur Papa <artur.papa@ufv.br>
2021-04-13 13:06:00 -0300

    Up

--- Tp2/src/main.c
+++ Tp2/src/main.c
@@ -6,11 +6,11 @@ int main()
     double tempo;
     clock_t fim,inicio;
 
-    long long int n = 1 << 15, i;
+    long long int n = 1 << 4, i;
  
     inicio=clock(); 
     for (i = 0; i < n; i++)
-        truthTable(n, 15);
+        truthTable(i, 4);
     fim=clock();
     tempo=((double)(fim-inicio))/CLOCKS_PER_SEC; 
     printf("Tempo Gasto = %lf segundos\n\n\n",tempo);
