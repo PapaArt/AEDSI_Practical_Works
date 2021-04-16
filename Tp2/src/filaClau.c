87af12be41d3f62a67896f846dc52f932eed121c
Artur Papa <artur.papa@ufv.br>
2021-04-16 15:02:14 -0300

    Up

--- Tp2/src/filaClau.c
+++ Tp2/src/filaClau.c
@@ -1,50 +1,41 @@
 #include "sat.h"
 
-void inicializaValor(TItem *item, int linha, int coluna, int valor, int posicao)
+
+void imprimeItem(TItem* pItem)
 {
-    item->coluna[0] = coluna;
-    item->coluna[1] = coluna;
-    item->coluna[2] = coluna;
-    item->linha = linha;
-    item->valor = valor;
-    for (int i = 0; i < 50; i++)
-    {
-        item->posicao[i] = posicao;
+    printf("%d %d %d\n", pItem->posicao1, pItem->posicao2, pItem->posicao3);
+    printf("%d %d %d\n", pItem->valor1, pItem->valor2, pItem->valor3);
+}
+
+void FLVazia(TipoLista *plv){ 
+    plv -> Primeiro = (TipoApontador) malloc(sizeof(TipoCelula));
+    plv -> Ultimo = plv -> Primeiro;
+    plv -> Primeiro -> Prox = NULL;
+}
+
+void Imprime(TipoLista plv){ 
+    TipoApontador Aux;
+    Aux = plv.Primeiro -> Prox;
+    while (Aux != NULL) { 
+        imprimeItem(&Aux->Item);
+        Aux = Aux->Prox;
     }
 }
 
-void FFVazia(TFila *pFila)
-{
-    pFila->pFrente = (Apontador)malloc(sizeof(TCelula));
-    pFila->pTras = pFila->pFrente;
-    pFila->pFrente->pProx = NULL;
-} /* FFVazia */
-
-int FEhVazia(TFila *pFila)
-{
-    return (pFila->pFrente == pFila->pTras);
-} /* FEhVazia */
-
-int FEnfileira(TFila *pFila, TItem *pItem)
-{
-    Apontador pNovo;
-    pNovo = (Apontador)malloc(sizeof(TCelula));
-    if (pNovo == NULL)
-        return 0;
-
-    pFila->pTras->pProx = pNovo;
-    pFila->pTras = pNovo;
-    pNovo->item = *pItem;
-    pNovo->pProx = NULL;
-    return 1;
-
-} /* FEnfileira */
+void Insere(TItem x, TipoLista *plv){ 
+    plv->Ultimo->Prox = (TipoApontador) malloc(sizeof(TipoCelula));
+    plv -> Ultimo = plv -> Ultimo -> Prox;
+    plv -> Ultimo -> Item = x;
+    plv -> Ultimo -> Prox = NULL;
+}
 
 //criar "percorrer" antes de adaptar a função abaixo
-void geraclauses(TCelula* pCell, int (*mat)[50], int N)
+void geraclauses(TipoLista lista1, int (*mat)[50], int N)
 {
+    TItem registro1;
+    FLVazia(&lista1);
     int C;
-    int randomic1, randomic2, randomic3;
+    int randomic1, randomic2, randomic3, test1, test2, test3;
     int i, j;
     C = (N / 3) * 2;
 
@@ -57,63 +48,56 @@ void geraclauses(TCelula* pCell, int (*mat)[50], int N)
     srand(time(0));
     for (i = 0; i < C; i++)
     {
-        pCell->item.coluna[0] = 0;
-        pCell->item.coluna[1] = 0;
-        pCell->item.coluna[2] = 0;
+        
 
         randomic1 = rand() % N;
-        pCell->item.coluna[0] = rand() % 3;
-        if (pCell->item.coluna[0] == 0)
+        test1 = rand() % 3;
+        if (test1 == 0)
         {
-            pCell->item.coluna[0]++;
+            test1++;
         }
-        mat[i][randomic1] = pCell->item.coluna[0];
+        mat[i][randomic1] = test1;
 
         randomic2 = rand() % N;
-        pCell->item.coluna[1] = rand() % 3;
-        if (pCell->item.coluna[1] == 0)
+        test2 = rand() % 3;
+        if (test2 == 0)
         {
-            pCell->item.coluna[1]++;
+            test2++;
         }
 
         while (randomic2 == randomic1)
         {
             randomic2 = rand() % N;
         }
-        mat[i][randomic2] = pCell->item.coluna[1];
+        mat[i][randomic2] = test2;
 
         randomic3 = rand() % N;
-        pCell->item.coluna[0] = rand() % 3;
-        if (pCell->item.coluna[2] == 0)
+        test3 = rand() % 3;
+        if (test3 == 0)
         {
-            pCell->item.coluna[2]++;
+            test3++;
         }
 
         while (randomic3 == randomic2 || randomic3 == randomic1)
         {
             randomic3 = rand() % N;
         }
-        mat[i][randomic3] = pCell->item.coluna[2];
-        // printf("%d %d %d\n", pCell->item.coluna[0], pCell->item.coluna[1], pCell->item.coluna[2]);
+        mat[i][randomic3] = test3;
+
+        registro1.posicao1 = randomic1;
+        registro1.posicao2 = randomic2;
+        registro1.posicao3 = randomic3;
+        registro1.valor1 = test1;
+        registro1.valor2 = test2;
+        registro1.valor3 = test3;
+        Insere(registro1, &lista1);
     }
-    for(i=0; i<C; i++){
-		for(j=0; j<N; j++){
-			printf("%d",mat[i][j]);
-		}printf("\n");
-    }
-    percorre(pCell, mat, C, N);
-}
-
-int FDesenfileira(TFila *pFila, TItem *pItem)
-{
-    Apontador pAux;
-    if (FEhVazia(pFila))
-        return 0;
-
-    pAux = pFila->pFrente;
-    pFila->pFrente = pFila->pFrente->pProx;
-    *pItem = pFila->pFrente->item;
-    free(pAux);
-
-    return 1;
+    // for(i=0; i<C; i++){
+	// 	for(j=0; j<N; j++){
+	// 		printf("%d",mat[i][j]);
+	// 	}printf("\n");
+    // }
+    printf("Imprimindo a lista...\n");
+    // imprimeFila(pFila);
+    Imprime(lista1);
 }
--- Tp2/src/sat.h
+++ Tp2/src/sat.h
@@ -5,8 +5,6 @@
 void semPreconceito();
 void truthTable(int n, int leng);
 //void verifica(TItem* pItem);
-int verifica(TFila* pFila);
-void percorre(TCelula* pCell, int (*mat)[50], int C, int N);
-void printaLinha();
+//int verifica(TFila* pFila);
 
 #endif 
