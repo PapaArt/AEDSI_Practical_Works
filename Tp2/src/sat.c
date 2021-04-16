4885150c8fcecf21e230826a7e3045ed93bbf223
Vinícius <omendes.vinicius@gmail.com>
2021-04-16 18:46:21 -0300

    commit on main

--- Tp2/src/sat.c
+++ Tp2/src/sat.c
@@ -20,13 +20,32 @@ void truthTable(int n, int leng, TItem* pItem)
         j++;
         bit >>= 1;
     }
-    j = pItem->posicao1;
-    // if (pItem->valor1 == 1)
-    // {
+    int jP = pItem->posicao1;
+    int kP = pItem->posicao2;
+    int lP= pItem->posicao3;
+    int jV = pItem->valor1;
+    int kV = pItem->valor2;
+    int lV= pItem->valor3;
+
+    if((jV == 2 || kV == 2 || lV == 2) && (coluna[jP] == 1 || coluna[kP] == 1 || coluna[lP] == 1)){
+        if(coluna[jP] == 0){
+            printf("( %sx%d |",(jV == 2 ? "!" : ""),(jP));
+        }if(coluna[jP]==1){
+            printf("( %sx%d |",(jV == 1 ? "!" : ""),(jP));
+        }if(coluna[kP] == 0){
+            printf(" %sx%d |",(kV == 2 ? "!" : ""),(kP));
+        }if(coluna[kP]==1){
+            printf(" %sx%d |",(kV == 1 ? "!" : ""),(kP));
+        }if(coluna[lP] == 0){
+            printf(" %sx%d ) & ",(lV == 2 ? "!" : ""),(lP));
+        }if(coluna[lP]==1){
+            printf(" %sx%d ) & ",(lV == 1 ? "!" : ""),(lP));
+        }
         
-    // }
+    }
+    // for(j=0; j<leng; j++)
+    //     printf("%d", coluna[j]);
     
-    printf("\n");
 }
 
 // int verifica(TFila* pFila)
