478757da137a5175d978df4b06c4100acaec796b
Vinícius <omendes.vinicius@gmail.com>
2021-04-17 17:40:51 -0300

    commit

--- Tp2/src/sat.c
+++ Tp2/src/sat.c
@@ -30,36 +30,29 @@ int truthTable(int n, int leng, TItem *pItem)
     int kV = pItem->valor2;
     int lV = pItem->valor3;
 
-    pos = pItem->posicao3;
-    colum = (leng - 1) - pos;
-    int potencia = pow(2, colum);
+    int expressao = 1 ;
+    int claus = 0;
+    if(jV == 2 && coluna[jP]==1){
+        claus = 1;
+    }if(jV == 1 && coluna[jP] == 0 ){
+        claus = 1;
+    }if(kV == 2 && coluna[kP]==1){
+        claus = 1;
+    }if(kV == 1 && coluna[kP] == 0 ){
+        claus = 1;
+    }if(lV == 2 && coluna[lP]==1){
+        claus = 1;
+    }if(lV == 1 && coluna[lP] == 0 ){
+        claus = 1;
+    }
 
-    if ((jV == 2 && coluna[jP] == 1) || (kV == 2 && coluna[kP] == 1) || (lV == 2 && coluna[lP] == 1) ||
-        (jV == 1 && coluna[jP] == 0) || (kV == 1 && coluna[kP] == 0) || (lV == 1 && coluna[lP] == 0))
-    {
-        if (coluna[jP] == 0)
-        {
-            printf("( %sx%d |", (jV == 2 ? "" : "!"), (jP));
-        }
-        if (coluna[jP] == 1)
-        {
-            printf("( %sx%d |", (jV == 1 ? "" : "!"), (jP));
-        }
-        if (coluna[kP] == 0)
-        {
-            printf(" %sx%d |", (kV == 2 ? "" : "!"), (kP));
-        }
-        if (coluna[kP] == 1)
-        {
-            printf(" %sx%d |", (kV == 1 ? "" : "!"), (kP));
-        }
-        if (coluna[lP] == 0)
-        {
-            printf(" %sx%d ) & ", (lV == 2 ? "" : "!"), (lP));
-        }
-        if (coluna[lP] == 1)
-        {
-            printf(" %sx%d ) & ", (lV == 1 ? "" : "!"), (lP));
-        }
+    if(claus == 0){
+        expressao = 0;
+    }
+
+    if(expressao == 1){
+        for(int i=0; i<leng;i++){
+            printf("%d",coluna[i]);
+        }printf("\n");
     }
 }
