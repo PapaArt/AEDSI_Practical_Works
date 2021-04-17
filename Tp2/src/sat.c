4e162c230ca493dd5fbd80800545e4e9d2d614d1
Artur Papa <artur.papa@ufv.br>
2021-04-17 17:57:36 -0300

    Up

--- Tp2/src/sat.c
+++ Tp2/src/sat.c
@@ -11,10 +11,10 @@ void semPreconceito()
 
 int truthTable(int n, int leng, TItem *pItem)
 {
-    int coluna[40];
+    long long int coluna[40];
     int pos = 0;
     int colum = 0;
-    int i,j = 0, k = 0, z = 0;
+    int i, j = 0, k = 0, z = 0;
     long long int bit = 1 << leng - 1;
     while (bit)
     {
@@ -22,36 +22,51 @@ int truthTable(int n, int leng, TItem *pItem)
         j++;
         bit >>= 1;
     }
-    int jP = (n - 1) - (pItem->posicao1);
+    int jP = pItem->posicao1;
     int kP = pItem->posicao2;
     int lP = pItem->posicao3;
     int jV = pItem->valor1;
     int kV = pItem->valor2;
     int lV = pItem->valor3;
 
-    int expressao = 1 ;
+    int expressao = 1;
     int claus = 0;
-    if(jV == 2 && coluna[jP]==1){
+    if (jV == 2 && coluna[jP] == 1)
+    {
         claus = 1;
-    }if(jV == 1 && coluna[jP] == 0 ){
+    }
+    if (jV == 1 && coluna[jP] == 0)
+    {
         claus = 1;
-    }if(kV == 2 && coluna[kP]==1){
+    }
+    if (kV == 2 && coluna[kP] == 1)
+    {
         claus = 1;
-    }if(kV == 1 && coluna[kP] == 0 ){
+    }
+    if (kV == 1 && coluna[kP] == 0)
+    {
         claus = 1;
-    }if(lV == 2 && coluna[lP]==1){
+    }
+    if (lV == 2 && coluna[lP] == 1)
+    {
         claus = 1;
-    }if(lV == 1 && coluna[lP] == 0 ){
+    }
+    if (lV == 1 && coluna[lP] == 0)
+    {
         claus = 1;
     }
 
-    if(claus == 0){
+    if (claus == 0)
+    {
         expressao = 0;
     }
 
-    if(expressao == 1){
-        for(int i=0; i<leng;i++){
-            printf("%d",coluna[i]);
-        }printf("\n");
-    }       
+    if (expressao == 1)
+    {
+        for (int i = 0; i < leng; i++)
+        {
+            printf("%lld", coluna[i]);
+        }
+        printf("\n");
+    }
 }
