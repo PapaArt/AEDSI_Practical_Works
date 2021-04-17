a3c70220fe6ed0bb24f95bbc9d37b8e862cdf582
Artur Papa <artur.papa@ufv.br>
2021-04-17 20:13:06 -0300

    Final

--- Tp2/graph.py
+++ Tp2/graph.py
@@ -3,9 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-x = np.linspace()
+x = np.linspace(-1.5,2,100)
+y = (2*((x**3)/3)) + (x**2)
+fig = plt.figure(figsize = (10, 6))
+
 plt.xlabel("Variaveis")
 plt.ylabel("Clausulas")
 plt.title("Complexidade assintotica")
+plt.grid()
+
 plt.plot(x, y)
 plt.show()
