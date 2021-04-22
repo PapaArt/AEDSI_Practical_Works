f17ea7fd7245e321ad0ab0de3f562eabeb1afc79
Artur Papa <artur.papa@ufv.br>
2021-04-22 14:49:17 -0300

    Up

--- Tp3/src/sorting.c
+++ Tp3/src/sorting.c
@@ -105,3 +105,53 @@ int quickSort(int arr[], int low, int high)
         quickSort(arr, pi + 1, high);
     }
 }
+
+void heapify(int arr[], int n, int i)
+{
+    int largest = i; // Initialize largest as root
+    int l = (2 * i) + 1; // left = 2*i+1
+    int r = (2 * i) + 1; // right = 2*i+1
+
+    // If left child is larger than root
+    if (l < n && arr[l] > arr[largest])
+        largest = l;
+    
+    // If right child is larger than largest so far
+    if (r < n && arr[r] > arr[largest])
+        largest = r;
+
+    // If largest is not root
+    if (largest != i)
+    {
+        swap(arr[i], arr[largest]);
+
+        // Recursively heapify the affected sub-tree
+        heapify(arr, n, largest);
+    }
+}
+
+int heapSort(int arr[], int n)
+{
+    int i;
+
+    // Build heap (rearrange array)
+    for (i = (n/2) - 1; i >= 0; i--)
+        heapify(arr, n, i);
+    
+    // One by one extract an element from heap
+    for (i = n-1; i > 0; i--)
+    {
+        // Move current root to end
+        swap(arr[0], arr[i]);
+
+        // Call max heapify on the reduced heap
+        heapify(arr, i, 0);
+    }
+}
+
+void  printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
--- Tp3/src/sorting.h
+++ Tp3/src/sorting.h
@@ -11,6 +11,10 @@ int selectionSort(int arr[], int n);
 int shellSort(int arr[], int n);
 int partition(int arr[], int low, int high);
 int quickSort(int arr[], int low, int high);
+void heapify(int arr[], int n, int i);
+int heapSort(int arr[], int n);
 
+// Funcao para imprimir um vetor de tamanho n
+void printArray(int arr[], int n);
 
 #endif
