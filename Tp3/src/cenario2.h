778a19974fccb9f826a9dc4521264ebd5a2dd208
Vinícius <omendes.vinicius@gmail.com>
2021-04-22 22:32:06 -0300

    up

---
+++ Tp3/src/cenario2.h
@@ -0,0 +1,32 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef CENARIO2_H
+#define CENARIO2_H
+
+typedef struct Registro{
+    
+    int id;
+    char string1[200],string2[200],string3[200],string4[200],string5[200]
+        ,string6[200],string7[200],string8[200],string9[200],string10[200];
+    int real_value1, real_value2, real_value3, real_value4;
+
+}Registro;
+
+typedef struct Celula* Apontador;
+typedef struct Celula{
+
+    Registro registro;
+    Apontador pProx;
+
+}TCelula;
+
+typedef struct{
+
+    Apontador pPrimeiro;
+    Apontador pUltimo;
+
+}TLista;
+
+#endif
