33f96667dc4a2ef816eb4f10e71b47892a73e932
Artur Papa <artur.papa@ufv.br>
2021-04-27 14:51:23 -0300

    up

--- Tp3/src/cenario2.h
+++ Tp3/src/cenario2.h
@@ -4,10 +4,9 @@
 
 typedef struct Registro{
     
-    int id;
-    char string1[200],string2[200],string3[200],string4[200],string5[200],string6[200],string7[200]
-        ,string8[200],string9[200],string10[200];
-    int real_value1, real_value2, real_value3, real_value4;
+    int chave;
+    char string[10][200];
+    float real_value[4];
 
 }Registro;
 
