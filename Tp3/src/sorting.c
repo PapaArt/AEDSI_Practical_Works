8e892197aa87e838fee778ec1ee24a686caef8fa
Artur Papa <artur.papa@ufv.br>
2021-04-30 14:56:18 -0300

    up

--- Tp3/src/sorting.c
+++ Tp3/src/sorting.c
@@ -106,49 +106,6 @@ int quickSort(int* arr, int low, int high)
     }
 }
 
-void heapify(int* arr, int n, int i)
-{
-    int largest = i; // Initialize largest as root
-    int l = (2 * i) + 1; // left = 2*i+1
-    int r = (2 * i) + 1; // right = 2*i+1
-
-    // If left child is larger than root
-    if (l < n && arr[l] > arr[largest])
-        largest = l;
-    
-    // If right child is larger than largest so far
-    if (r < n && arr[r] > arr[largest])
-        largest = r;
-
-    // If largest is not root
-    if (largest != i)
-    {
-        swap(&arr[i], &arr[largest]);
-
-        // Recursively heapify the affected sub-tree
-        heapify(arr, n, largest);
-    }
-}
-
-int heapSort(int* arr, int n)
-{
-    int i;
-
-    // Build heap (rearrange array)
-    for (i = (n/2) - 1; i >= 0; i--)
-        heapify(arr, n, i);
-    
-    // One by one extract an element from heap
-    for (i = n-1; i > 0; i--)
-    {
-        // Move current root to end
-        swap(&arr[0], &arr[i]);
-
-        // Call max heapify on the reduced heap
-        heapify(arr, i, 0);
-    }
-}
-
 void merge(int* arr, int l, int m, int r)
 {
     int i, j, k;
