a4a91ad5b947fd7204c30ce9ce4ebd0d4af53d81
Vinícius <omendes.vinicius@gmail.com>
2021-05-01 21:10:20 -0300

    up

--- Tp3/src/cenarios.c
+++ Tp3/src/cenarios.c
@@ -1,13 +1,13 @@
 #include "cenarios.h"
 
-void FLVazia(TipoLista* item)
+void FLVazia(TipoLista *item)
 {
     item->pPrimeiro = (Apontador)malloc(sizeof(TipoCelula));
     item->pUltimo = item->pPrimeiro;
     item->pPrimeiro->pProx = NULL;
 }
 
-void Insere(Registro x, TipoLista* item)
+void Insere(Registro x, TipoLista *item)
 {
     item->pUltimo->pProx = (Apontador)malloc(sizeof(TipoCelula));
     item->pUltimo = item->pUltimo->pProx;
@@ -15,11 +15,37 @@ void Insere(Registro x, TipoLista* item)
     item->pUltimo->pProx = NULL;
 }
 
-void random_array(long long int* arr, int resposta)
+void random_array(long long int *arr, int resposta)
 {
     int i;
     for (i = 0; i < resposta; i++)
     {
-        arr[i] = rand()%101;
+        arr[i] = rand() % 101;
+    }
+}
+
+void random_id(Registro *item)
+{
+    item->chave = rand() % 101;
+}
+
+void random_char(Registro *item)
+{
+    int i, j;
+    for (i = 0; i < 10; i++)
+    {
+        for (j = 0; j < 200; j++)
+        {
+            item->string[i][j] = 'a' + (char)(rand() % 26);
+        }
+    }
+}
+
+void random_realvalue(Registro *item)
+{
+
+    for (int i = 0; i < 4; i++)
+    {
+        
     }
 }
--- Tp3/src/cenarios.h
+++ Tp3/src/cenarios.h
@@ -28,5 +28,8 @@ typedef struct{
 void FLVazia(TipoLista *item);
 void Insere(Registro x, TipoLista *item);
 void random_array(long long int* arr, int resposta);
+void random_id(Registro* item);
+void random_char(Registro* item);
+void random_realvalue(Registro* item);
 
 #endif
