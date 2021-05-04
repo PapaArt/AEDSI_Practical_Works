0fbbf7d8d095489f2492930583244357599f0754
Artur Papa <artur.papa@ufv.br>
2021-05-04 14:41:14 -0300

    Up

--- Tp3/src/menu.c
+++ Tp3/src/menu.c
@@ -40,18 +40,25 @@ int menu()
             if (escolha[1] == 1)
             {
                 bubbleSort(arr, resposta);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 2){
                 selectionSort(arr, resposta);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 3){
                 insertionSort(arr, resposta);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 4){
                 shellSort(arr, resposta);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 5){
                 quickSort(arr, 0, resposta - 1);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 6){
                 mergeSort(arr, 0, resposta - 1, 1);
+                printf("\nArquivo gerado com sucesso!!!\n");
             } else if(escolha[1] == 7){
                 radixSort(arr, resposta);
+                printf("\nArquivo gerado com sucesso!!!\n");
             }
 
             printArray(arr, resposta);
