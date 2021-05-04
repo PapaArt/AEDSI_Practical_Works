16b11d75cf54acc9298786cafd597662f8c9ba43
Joao-Robert0 <80291396+Joao-Robert0@users.noreply.github.com>
2021-05-04 18:18:37 -0300

    Update sorting.h

--- Tp3/src/sorting.h
+++ Tp3/src/sorting.h
@@ -8,17 +8,17 @@
 
 void swap(long long int* xp, long long int* yp);
 //As funcoes precisam ser int para retornar o contador
-int* bubbleSort(long long int* arr, int n);
-int* selectionSort(long long int* arr, int n);
-int* insertionSort(long long int* arr, int n);
-int* shellSort(long long int* arr, int n);
+void bubbleSort(long long int* arr, int n,int flag);
+void selectionSort(long long int* arr, int n,int flag);
+void insertionSort(long long int* arr, int n,int flag);
+void shellSort(long long int* arr, int n,int flag);
 int partition(long long int* arr, int low, int high);
-int quickSort(long long int* arr, int low, int high);
+void quickSort(long long int* arr, int low, int high,int flag);
 void merge(long long int* arr, int l, int m, int r,double *comp,double mov,int flag);
 void mergeSort(long long int* arr, int l, int r,int flag);
 int getMax(long long int* arr, int n);
-void countSort(long long int* arr, int n, int exp);
-int radixSort(long long int* arr, int n);
+void countSort(long long int* arr, int n, int exp,int flag);
+void radixSort(long long int* arr, int n,int flag);
 
 // Funcao para imprimir um vetor de tamanho n
 void printArray(long long int* arr, int n);
